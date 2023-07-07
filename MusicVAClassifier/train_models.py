ed7746b72138a7c237ecebb221d040a03d80e502
Yunikyon <anaritamsmartins@gmail.com>
2023-07-07 22:20:53 +0100

    new VA model with keras and optuna

--- MusicVAClassifier/train_models.py
+++ MusicVAClassifier/train_models.py
@@ -1,11 +1,18 @@
 from __future__ import unicode_literals
 import csv
+
+import matplotlib.pyplot as plt
+import optuna
 import youtube_dl
 import numpy as np
 import pandas as pd
 import librosa
+from keras import Model
+from keras.optimizers import SGD
 from sklearn.svm import SVR, NuSVR
 from sklearn.model_selection import train_test_split
+from keras.layers import Input, Dense, Dropout
+import optuna.visualization as vis
 from sklearn.metrics import mean_squared_error
 import joblib
 
@@ -105,173 +112,243 @@ def build_model():
     dataset = pd.read_csv("training_dataset.csv")
     print("Dataset read")
 
-    # Extract Common features using librosa
+    # --- Extract Common features using librosa
     # Mel-frequency cepstral coefficients (MFCCs)
-    mfcc = np.array([librosa.feature.mfcc(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-                     dataset["Music_name"]])
+    # TODO - define fmin and fmax - but see first the average
+    mfcc = np.array([librosa.feature.mfcc(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                          sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+                     for file in dataset["Music_name"]])
+    print(np.min(mfcc))  # -667.08264
+    print(np.max(mfcc))  # 260.12265
+    mfcc = np.around(np.interp(mfcc, (-700, 300), (0, 1)), decimals=3)
     print("Mfcc done!")
+
     # Spectral centroid
+    # TODO - define fmin and fmax - but see first the average
     cent = np.array(
         [librosa.feature.spectral_centroid(y=librosa.load('MuVi_musics/' + file, duration=150)[0]) for file in
          dataset["Music_name"]])
+    print(np.min(cent))  # 0.0
+    print(np.max(cent))  # 8504.014129762603
+    cent = np.around(np.interp(cent, (0, 8550), (0, 1)), decimals=3)
     print("cent done!")
+
+    # TODO - Acho que Range: [0; 1]
+    # TODO - normalizar para 3 casas decimais
     # Zero-crossing rate
     zcr = np.array(
         [librosa.feature.zero_crossing_rate(y=librosa.load('MuVi_musics/' + file, duration=150)[0]) for file in
          dataset["Music_name"]])
+    print(np.min(zcr))  # 0.0
+    print(np.max(zcr))  # 0.82666015625
+    zcr = np.around(zcr, decimals=3)
     print("zcr done!")
 
-    # Extract Valence features using librosa
-    # Chroma features
-    chroma_cqt = np.array(
-        [librosa.feature.chroma_cqt(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-         dataset["Music_name"]])
-    print("Chroma_cqt done!")
-    chroma_stft = np.array(
-        [librosa.feature.chroma_stft(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-         dataset["Music_name"]])
-    print("chroma_stft done!")
-    chroma_cens = np.array(
-        [librosa.feature.chroma_cens(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-         dataset["Music_name"]])
-    print("chroma_cens done!")
-    # Spectral rolloff
-    rolloff = np.array(
-        [librosa.feature.spectral_rolloff(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-         dataset["Music_name"]])
-    print("rolloff done!")
-    # rms
-    rms = np.array(
-        [librosa.feature.rms(y=librosa.load('MuVi_musics/' + file, duration=150)[0]) for file in
-         dataset["Music_name"]])
-    print("rms done!")
 
-    # Extract Arousal features using librosa
+    # --- Extract Valence features using librosa
+    # Chroma features
+    # TODO - Acho que Range: [0; 1]
+    # TODO - normalizar para 3 casas decimais
+    chroma_cqt = np.array(
+        [librosa.feature.chroma_cqt(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                    sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+         for file in dataset["Music_name"]])
+    print(np.min(chroma_cqt))  # 0.0049088965
+    print(np.max(chroma_cqt))  # 1.0
+    chroma_cqt = np.around(chroma_cqt, decimals=3)
+    print("Chroma_cqt done!")
+
+    # Range: [0; 1]
+    # TODO - normalizar para 3 casas decimais
+    chroma_stft = np.array(
+        [librosa.feature.chroma_stft(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                     sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+         for file in dataset["Music_name"]])
+    print(np.min(chroma_stft))  # 0.0
+    print(np.max(chroma_stft))  # 1.0
+    chroma_stft = np.around(chroma_stft, decimals=3)
+    print("chroma_stft done!")
+
+    # Range: [0; 1]
+    # TODO - normalizar para 3 casas decimais
+    chroma_cens = np.array(
+        [librosa.feature.chroma_cens(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                     sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+         for file in dataset["Music_name"]])
+    print(np.min(chroma_cens))  # 0.0
+    print(np.max(chroma_cens))  # 1.0
+    chroma_cens = np.around(chroma_cens, decimals=3)
+    print("chroma_cens done!")
+
+    # Spectral rolloff
+    # TODO - define min and max - but see first the average
+    rolloff = np.array(
+        [librosa.feature.spectral_rolloff(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                          sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+         for file in dataset["Music_name"]])
+    print(np.min(rolloff))  # 0.0
+    print(np.max(rolloff))  # 10562.0361328125
+    rolloff = np.around(np.interp(rolloff, (0, 10800), (0, 1)), decimals=3)
+    print("rolloff done!")
+
+    # --- Extract Arousal features using librosa
     # Spectral contrast
+    # TODO - define fmin and fmax - but see first the average - I think it goes from 0 to 65
     spectral_contrast = np.array(
-        [librosa.feature.spectral_contrast(y=librosa.load('MuVi_musics/' + file, duration=150)[0], sr=44100) for file in
-         dataset["Music_name"]])
+        [librosa.feature.spectral_contrast(y=librosa.load('MuVi_musics/' + file, duration=150)[0],
+                                           sr=librosa.load('MuVi_musics/' + file, duration=150)[1])
+         for file in dataset["Music_name"]])
+    print(np.min(spectral_contrast))  # 0.17194677838910621
+    print(np.max(spectral_contrast))  # 72.256236009688
+    spectral_contrast = np.around(np.interp(spectral_contrast, (0, 100), (0, 1)), decimals=3)
     print("Spectral_contrast done!")
 
-    # Convert the features to NumPy arrays with the same shape
-    # 1. Get the max length
-    max_shape_valence = max(mfcc.shape[1], cent.shape[1], zcr.shape[1], rolloff.shape[1], chroma_cqt.shape[1],
-                            chroma_stft.shape[1], chroma_cens.shape[1], rms.shape[1])
-    max_shape_arousal = max(mfcc.shape[1], cent.shape[1], zcr.shape[1], spectral_contrast.shape[1])
+    # Create individual DataFrames for each array
+    dataframe_mfcc = pd.DataFrame(mfcc.reshape(mfcc.shape[0], -1))
+    dataframe_cent = pd.DataFrame(cent.reshape(cent.shape[0], -1))
+    dataframe_zcr = pd.DataFrame(zcr.reshape(zcr.shape[0], -1))
+    dataframe_chroma_cqt = pd.DataFrame(chroma_cqt.reshape(chroma_cqt.shape[0], -1))
+    dataframe_chroma_stft = pd.DataFrame(chroma_stft.reshape(chroma_stft.shape[0], -1))
+    dataframe_chroma_cens = pd.DataFrame(chroma_cens.reshape(chroma_cens.shape[0], -1))
+    dataframe_rolloff = pd.DataFrame(rolloff.reshape(rolloff.shape[0], -1))
+    dataframe_spectral_contrast = pd.DataFrame(spectral_contrast.reshape(spectral_contrast.shape[0], -1))
 
-    # 2.1 Convert Valence to the same shape
-    if max_shape_valence - mfcc.shape[1] != 0:
-        mfcc_valence = np.pad(mfcc, ((0, 0), (0, max_shape_valence - mfcc.shape[1]), (0, 0)), mode='constant',
-                              constant_values=0)
-    else:
-        mfcc_valence = mfcc
-    if max_shape_valence - cent.shape[1] != 0:
-        cent_valence = np.pad(cent, ((0, 0), (0, max_shape_valence - cent.shape[1]), (0, 0)), mode='constant',
-                              constant_values=0)
-    else:
-        cent_valence = cent
-    if max_shape_valence - zcr.shape[1] != 0:
-        zcr_valence = np.pad(zcr, ((0, 0), (0, max_shape_valence - zcr.shape[1]), (0, 0)), mode='constant',
-                             constant_values=0)
-    else:
-        zcr_valence = zcr
-    if max_shape_valence - rolloff.shape[1] != 0:
-        rolloff = np.pad(rolloff, ((0, 0), (0, max_shape_valence - rolloff.shape[1]), (0, 0)), mode='constant',
-                         constant_values=0)
-    if max_shape_valence - chroma_cqt.shape[1] != 0:
-        chroma_cqt = np.pad(chroma_cqt, ((0, 0), (0, max_shape_valence - chroma_cqt.shape[1]), (0, 0)), mode='constant',
-                            constant_values=0)
-    if max_shape_valence - chroma_stft.shape[1] != 0:
-        chroma_stft = np.pad(chroma_stft, ((0, 0), (0, max_shape_valence - chroma_stft.shape[1]), (0, 0)),
-                             mode='constant',
-                             constant_values=0)
-    if max_shape_valence - chroma_cens.shape[1] != 0:
-        chroma_cens = np.pad(chroma_cens, ((0, 0), (0, max_shape_valence - chroma_cens.shape[1]), (0, 0)),
-                             mode='constant',
-                             constant_values=0)
-    if max_shape_valence - rms.shape[1] != 0:
-        rms = np.pad(rms, ((0, 0), (0, max_shape_valence - rms.shape[1]), (0, 0)),
-                     mode='constant',
-                     constant_values=0)
+    # Concatenate the DataFrames along the column axis (axis=1)
+    dataframe_valence = pd.concat([dataframe_mfcc, dataframe_cent, dataframe_zcr, dataframe_chroma_cqt,
+                                   dataframe_chroma_stft, dataframe_chroma_cens, dataframe_rolloff], axis=1)
 
-    # 2.2 Convert Arousal to the same shape
-    if max_shape_arousal - mfcc.shape[1] != 0:
-        mfcc_arousal = np.pad(mfcc, ((0, 0), (0, max_shape_arousal - mfcc.shape[1]), (0, 0)), mode='constant',
-                              constant_values=0)
-    else:
-        mfcc_arousal = mfcc
-    if max_shape_arousal - cent.shape[1] != 0:
-        cent_arousal = np.pad(cent, ((0, 0), (0, max_shape_arousal - cent.shape[1]), (0, 0)), mode='constant',
-                              constant_values=0)
-    else:
-        cent_arousal = cent
-    if max_shape_arousal - zcr.shape[1] != 0:
-        zcr_arousal = np.pad(zcr, ((0, 0), (0, max_shape_arousal - zcr.shape[1]), (0, 0)), mode='constant',
-                             constant_values=0)
-    else:
-        zcr_arousal = zcr
-    if max_shape_arousal - spectral_contrast.shape[1] != 0:
-        spectral_contrast = np.pad(spectral_contrast,
-                                   ((0, 0), (0, max_shape_arousal - spectral_contrast.shape[1]), (0, 0)),
-                                   mode='constant',
-                                   constant_values=0)
+    dataframe_arousal = pd.concat([dataframe_mfcc, dataframe_cent, dataframe_zcr, dataframe_spectral_contrast], axis=1)
 
     print("Concatenating...")
-    # Concatenate all features into one matrix for valence
-    x_valence = np.concatenate(
-        (mfcc_valence, cent_valence, zcr_valence, chroma_cqt, chroma_stft, chroma_cens, rolloff, rms), axis=2).reshape(
-        len(dataset), -1)
-    y_valence = dataset["Valence"].values
-
-    # Concatenate all features into one matrix for arousal
-    x_arousal = np.concatenate((mfcc_arousal, cent_arousal, zcr_arousal, spectral_contrast), axis=2).reshape(
-        len(dataset), -1)
-    y_arousal = dataset["Arousal"].values
+    y_valence = np.around(np.interp(dataset["Valence"].values, (-1, 1), (0, 1)), decimals=3)
+    y_arousal = np.around(np.interp(dataset["Arousal"].values, (-1, 1), (0, 1)), decimals=3)
 
     print("Train and test split...")
     # Split data into training and testing sets for Valence
-    x_train_valence, x_test_valence, y_train_valence, y_test_valence = train_test_split(x_valence, y_valence,
+    x_train_valence, x_test_valence, y_train_valence, y_test_valence = train_test_split(dataframe_valence, y_valence,
                                                                                         test_size=0.2, random_state=42)
-    x_train_arousal, x_test_arousal, y_train_arousal, y_test_arousal = train_test_split(x_arousal, y_arousal,
+    x_train_arousal, x_test_arousal, y_train_arousal, y_test_arousal = train_test_split(dataframe_arousal, y_arousal,
                                                                                         test_size=0.2, random_state=42)
 
-    print("Training...")
+    # --- Valence Model With Optuna
+    study = optuna.create_study(direction='minimize')  # or 'maximize' if optimizing accuracy
+    study.optimize(lambda trial: objective(trial, x_test_valence, x_train_valence, y_test_valence, y_train_valence,
+                                           'valence'), n_trials=5)
 
-    # Train SVM classifier for valence
-    svm_valence = NuSVR(kernel='rbf', nu=0.5, gamma='scale')  # Valence score = 0.3364867410372002
-    svm_valence.fit(x_train_valence, y_train_valence)
-    # svm_valence = SVR(kernel='rbf', C=1, gamma='scale') # Valence score = 0.32006080875792065
-    # svm_valence = RandomForestRegressor(n_estimators=100, random_state=42) # Valence score = 0.18499682846047716
-    # svm_valence = SVR(kernel='linear') # Valence score = 0.2957333921408458
+    # Plot and save the optimization history
+    # optuna_history = study.trials_dataframe()
+    fig = vis.plot_optimization_history(study)
+    fig.write_image("valence_optuna_history.png")
 
-    # Evaluate valence model
-    y_pred_valence = svm_valence.predict(x_test_valence)
-    mse = mean_squared_error(y_test_valence, y_pred_valence)
-    print("Valence MSE: ", mse)
-    print("Valence RMSE: ", mse * (1 / 2.0))
+    # Plot and save the slice plot
+    fig = vis.plot_slice(study)
+    fig.write_image("valence_optuna_slice_plot.png")
 
-    # Save valence model
-    model_file = "../models/svm_valence_classifier.pkl"
-    joblib.dump(svm_valence, model_file)
+    best_trial = study.best_trial
+    best_learning_rate = best_trial.params['learning_rate']
+    best_num_units = best_trial.params['num_units']
+    best_dropout_rate = best_trial.params['dropout_rate']
 
-    # Train SVM classifier for arousal
-    svm_arousal = SVR(kernel='linear', C=1)  # Arousal score = 0.18566561362623968
-    svm_arousal.fit(x_train_arousal, y_train_arousal)
-    # svm_arousal = SVR(kernel='rbf', C=1, gamma='scale') # Arousal score = 0.1672412704940316
-    # svm_arousal = RandomForestRegressor(n_estimators=100, random_state=42) # Arousal score = -0.1780557406683747
-    # svm_arousal = NuSVR(kernel='rbf', nu=0.5, gamma='scale') # Arousal score = 0.17019876993624228
+    best_metric, best_model = train_model(x_test_valence, x_train_valence, y_test_valence, y_train_valence, 'valence',
+                                          best_learning_rate, best_num_units, best_dropout_rate)
 
-    # Evaluate arousal model
-    y_pred_arousal = svm_arousal.predict(x_test_arousal)
-    mse = mean_squared_error(y_test_arousal, y_pred_arousal)
-    print("Arousal MSE: ", mse)
-    print("Arousal RMSE: ", mse * (1 / 2.0))
+    print('Best value: {:.5f}'.format(best_metric))
+    print('Best parameters: {}'.format(best_trial.params))
 
-    # Save arousal model
-    model_file = "../models/svm_arousal_classifier.pkl"
-    joblib.dump(svm_arousal, model_file)
+    # Save the best model
+    best_model.save("../models/valence_model.h5")
+
+    # --- Arousal Model With Optuna
+    study_arousal = optuna.create_study(direction='minimize')  # or 'maximize' if optimizing accuracy
+    study_arousal.optimize(lambda trial: objective(trial, x_test_arousal, x_train_arousal, y_test_arousal,
+                                                   y_train_arousal, 'arousal'), n_trials=5)
+
+    # Plot and save the optimization history
+    # optuna_history = study.trials_dataframe()
+    fig_arousal = vis.plot_optimization_history(study_arousal)
+    fig_arousal.write_image("arousal_optuna_history.png")
+
+    # Plot and save the slice plot
+    fig_arousal = vis.plot_slice(study_arousal)
+    fig_arousal.write_image("arousal_optuna_slice_plot.png")
+
+    best_trial_arousal = study_arousal.best_trial
+    best_learning_rate_arousal = best_trial_arousal.params['learning_rate']
+    best_num_units_arousal = best_trial_arousal.params['num_units']
+    best_dropout_rate_arousal = best_trial_arousal.params['dropout_rate']
+
+    best_metric_arousal, best_model_arousal = train_model(x_test_arousal, x_train_arousal, y_test_arousal,
+                                                          y_train_arousal, 'arousal', best_learning_rate_arousal,
+                                                          best_num_units_arousal, best_dropout_rate_arousal)
+
+    print('Best value: {:.5f}'.format(best_metric_arousal))
+    print('Best parameters: {}'.format(best_trial_arousal.params))
+
+    # Save the best mode
+    best_model_arousal.save("../models/arousal_model.h5")
+
+
+def objective(trial, x_test, x_train, y_test, y_train, characteristic):
+    # Define the hyperparameters to be optimized
+    learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
+    num_units = trial.suggest_int('num_units', 32, 512)
+    dropout_rate = trial.suggest_uniform('dropout_rate', 0.0, 0.5)
+
+    # Train the model and obtain the validation metric
+    metric, _ = train_model(x_test, x_train, y_test, y_train, characteristic, learning_rate, num_units, dropout_rate)
+
+    # 5. Evaluate the model on the test data
+    # evaluation_results = model.evaluate(x_test, y_test)
+
+    # Print the metric values during evaluation
+    # print(f"{characteristic} evaluation results: ")
+    # for metric_name, metric_value in zip(model.metrics_names, evaluation_results):
+    #     print(metric_name + ": " + str(metric_value))
+
+    # Return the validation metric as the objective value to be optimized (minimized)
+    return metric
+
+def train_model(x_test, x_train, y_test, y_train, characteristic, learning_rate, num_units, dropout_rate):
+    print(f"Training {characteristic}...")
+    # Define the input shape
+    input_shape = (x_train.shape[1],)
+
+    # Define the inputs
+    inputs = Input(shape=input_shape)
+
+    # Define the hidden layer with one layer and num_units neurons
+    hidden_layer = Dense(num_units, activation='relu')(inputs)
+    hidden_layer = Dropout(dropout_rate)(hidden_layer)
+
+    # Define the output layer with 1 neuron
+    outputs = Dense(1, activation='sigmoid')(hidden_layer)
+
+    # Create the model
+    model = Model(inputs=inputs, outputs=outputs)
+
+    # Compile the model with the desired learning rate
+    optimizer = SGD(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer, loss="mse", metrics=['mean_absolute_percentage_error'])
+
+    # Train the model
+    epochs = 100
+    batch_size = 16
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=0)
+
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['mean_absolute_percentage_error'])
+    # plt.title('Model Training History')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Value')
+    # plt.legend(['Loss', 'Mean Absolute Percentage Error'], loc='upper right')
+    # plt.savefig(f'{characteristic}_training_history.png')
+    # plt.close()
+
+    # Return the metric values for each epoch during training
+    return history.history['mean_absolute_percentage_error'][-1], model
 
 
 # download_mu_vi_musics()
-build_model()
+if __name__ == '__main__':
+    build_model()
 
